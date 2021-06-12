70ef38d4f8d02ebd3cd5406d50f303ba4c72370f
nash <nash@iffy.me>
2021-06-13 01:23:10 +0800

    dstask

--- .ext.zsh
+++ .ext.zsh
@@ -2,6 +2,14 @@ if [[ ! "$PATH" == */opt/bin* && -d /opt/bin ]]; then
     export PATH=/opt/bin:$PATH
 fi
 
+if (( $+commands[dstask])); then
+    alias q=dstask
+    _dstask() {
+        compadd -- $(dstask _completions "${words[@]}")
+    }
+    compdef _dstask dstask
+fi
+
 # sources $CFG/.fzf/init.zsh
 
 sources $CFG/.user.zsh
