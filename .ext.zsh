1f235da1832e7cfead2882a00bef8ca30c3c5c02
nash <nash@iffy.me>
2022-03-10 17:34:45 +0800

    xkb.zsh

--- .ext.zsh
+++ .ext.zsh
@@ -2,16 +2,11 @@ if [[ ! "$PATH" == */opt/bin* && -d /opt/bin ]]; then
     export PATH=/opt/bin:$PATH
 fi
 
-if (( $+commands[dstask])); then
-    alias q=dstask
-    _dstask() {
-        compadd -- $(dstask _completions "${words[@]}")
-    }
-    compdef _dstask dstask
-fi
 
 # sources $CFG/.fzf/init.zsh
 
+sources $CFG/.dstask.zsh
+sources $CFG/.xkb.zsh
 sources $CFG/../.user.zsh
 sources $HOME/.test.zsh
 sources $CFG/../.mutagen-alias
