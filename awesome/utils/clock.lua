27dd1acb05bad7f82fb885f6e56a9b38d28f4a84
nash <nash@iffy.me>
2022-03-15 01:26:22 +0800

    .

--- awesome/utils/clock.lua
+++ awesome/utils/clock.lua
@@ -3,6 +3,16 @@ local wibox = require'wibox'
 local awful = require'awful'
 local beautiful = require'beautiful'
 local gears = require'gears'
+local naughty = require'naughty'
+
+
+local dbg = function(x)
+    local aaa = ""
+    for k, a in ipairs(x) do
+        aaa = aaa .. ', ' .. k .. ' = ' .. tostring(a)
+    end
+    naughty.notify{text=aaa}
+end
 
 local clock = {
     forced_num_cols = 3,
@@ -24,6 +34,8 @@ for k, v in ipairs(group) do
         group[k] = v * clock.forced_num_cols + group[k - 1]
     end
 end
+
+-- dbg(group)
 
 local color = {}
 local color_set = {'#dde175', '#88b555'}
@@ -60,20 +72,50 @@ function tobitarray(r, t, len)
     end
 end
 
+local time_names = {'y', 'm', 'd', 'w', 'H', 'M', 'S'}
+local agot = {}
+local agob = {}
+for _, v in ipairs(time_names) do
+    local t = tonumber(os.date('%'..v))
+    table.insert(agot, t)
+    tobitarray(agob, t, v == 'w' and 3 or 6)
+end
+for i = 1, total do
+    clock[i].checked = agob[i]
+end
+
 gears.timer {
     timeout   = 1,
     call_now  = true,
     autostart = true,
     callback  = function()
-        local vls = {}
-        for k, v in ipairs{'y', 'm', 'd', 'w', 'H', 'M', 'S'} do
-            tobitarray(vls, tonumber(os.date('%'..v)), v == 'w' and 3 or 6)
-        end
-        for i = 1, total do
-            if clock[i].checked ~= vls[i]
-            then clock[i].checked = vls[i]
+        local begin_bit = 0
+        local bits = {}
+        for i = #time_names, 1, -1 do
+            local t = tonumber(os.date('%'..time_names[i]))
+            if t == agot[i] then
+                break
+            end
+            agot[i] = t
+            begin_bit = group[i-1]
+            for x = 0, time_names[i] == 'w' and 3 or 6, 1 do
+                table.insert(bits, 1, (t & (1 << x)) ~= 0)
             end
         end
+        for i = begin_bit + 1, total, 1 do
+            clock[i].checked = bits[i - begin_bit]
+        end
+        --dbg{begin_bit, total}
+        --dbg(bits)
+        --local vls = {}
+        --for k, v in ipairs(time_names) do
+        --    tobitarray(vls, tonumber(os.date('%'..v)), v == 'w' and 3 or 6)
+        --end
+        --for i = 1, total do
+        --    if clock[i].checked ~= vls[i]
+        --    then clock[i].checked = vls[i]
+        --    end
+        --end
     end
 }
 
