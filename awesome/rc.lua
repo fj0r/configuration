a2c288e6634762874af5ace3d595f5d358fea1c9
nash <nash@iffy.me>
2022-03-16 22:30:54 +0800

    awesomewm border_color

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -443,8 +443,13 @@ client.connect_signal("mouse::enter", function(c)
     c:emit_signal("request::activate", "mouse_enter", {raise = false})
 end)
 
-client.connect_signal("focus", function(c) c.border_color = conf.theme.border.focus
-                                                        and "#" .. conf.theme.border.focus
-                                                        or beautiful.border_focus end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+client.connect_signal("focus", function(c)
+    c.border_color = conf.theme.border.focus and "#" .. conf.theme.border.focus
+    or beautiful.border_focus
+end)
+client.connect_signal("unfocus", function(c)
+    c.border_color = conf.theme.border.normal
+    and "#" .. conf.theme.border.normal
+    or beautiful.border_normal
+end)
 -- }}}
