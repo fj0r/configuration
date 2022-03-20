930a6008c203e778b252299e11cefaf7d502570f
nash <nash@iffy.me>
2022-03-20 11:34:48 +0800

    recovery

--- awesome/utils/init.lua
+++ awesome/utils/init.lua
@@ -5,5 +5,6 @@ return {
     rotate = require('utils.rotate'),
     script_path = require('utils.script_path'),
     mk_taglist = require('utils.taglist'),
+    run_once = require('utils.run_once'),
     centerwork = require('utils.centerwork')
 }
