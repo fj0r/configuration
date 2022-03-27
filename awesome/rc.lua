fe4deba9403a68377b38d11b42e7c06af0223b1a
nash <nash@iffy.me>
2022-03-27 21:48:53 +0800

    awesomewm: exit fn keys

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -253,11 +253,11 @@ clientkeys = keys.client {
 -- Bind all key numbers to tags.
 -- Be careful: we use keycodes to make it work on any keyboard layout.
 -- This should map on the top row of your keyboard, usually 1 to 9.
-for i = 1, 12 do
+for i = 1, 10 do
     globalkeys = gears.table.join(globalkeys,
         -- View tag only.
         -- default: { modkey }, "#" .. i + 9
-        awful.key({ }, "F" .. i,
+        awful.key({ modkey }, "#" .. i + 9 ,
                   function ()
                         local screen = awful.screen.focused()
                         local tag = screen.tags[i]
@@ -268,7 +268,7 @@ for i = 1, 12 do
                   {description = "view tag #"..i, group = "tag"}),
         -- Move client to tag.
         -- default: { modkey, "Shift" }, "#" .. i + 9
-        awful.key({ "Shift" }, "F" .. i,
+        awful.key({ modkey }, "F" .. i,
                   function ()
                       if client.focus then
                           local tag = client.focus.screen.tags[i]
@@ -290,7 +290,7 @@ for i = 1, 12 do
                   end,
                   {description = "toggle tag #" .. i, group = "tag"}),
         -- Toggle tag on focused client.
-        awful.key({ modkey }, "F" .. i,
+        awful.key({ "Shift" }, "F" .. i,
                   function ()
                       if client.focus then
                           local tag = client.focus.screen.tags[i]
