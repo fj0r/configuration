3b700dd027a1c05d2ed208d036f0b6d33621da74
fj0r <fj0rd@qq.com>
2026-01-08 14:24:55 +0800

    Add helix unshare-push command and update nushell subproject

--- helix/init.scm
+++ helix/init.scm
@@ -4,5 +4,5 @@
 (define-language "scheme"
                  (language-servers '("steel-language-server")))
 
-(require "mattwparas-helix-package/helix.scm")
+;;(require "mattwparas-helix-package/helix.scm")
 ;;(require "mattwparas-helix-package/cogs/file-tree.scm")
